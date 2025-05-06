127a1869e1e4405f0f6d978f14b065432dd6bf37
aaron acuña <aaronacu_85@hotmail.com>
2025-05-05 22:22:48 -0500

    feat: add main.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:helptechmobileapp/IAM/views/login.dart';
+
+import 'IAM/views/home.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatelessWidget {
+
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Help Tech',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Home(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const Home(),
+        '/login': (context) => const Login()
+      },
+    );
+  }
+}
