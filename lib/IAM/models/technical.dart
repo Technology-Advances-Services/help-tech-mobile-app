7f1569dff27a2bb3fdbd566aa63a63c90d2c08c3
aaron acuña <aaronacu_85@hotmail.com>
2025-05-05 22:29:53 -0500

    feat(IAM): added technical model.

---
+++ lib/IAM/models/technical.dart
@@ -0,0 +1,30 @@
+class Technical {
+
+  String id;
+  int specialtyId;
+  int districtId;
+  String profileUrl;
+  String firstname;
+  String lastname;
+  int age;
+  String genre;
+  int phone;
+  String email;
+  String code;
+  String availability;
+
+  Technical({
+    this.id = '',
+    this.specialtyId = 0,
+    this.districtId = 0,
+    this.profileUrl = '',
+    this.firstname = '',
+    this.lastname = '',
+    this.age = 0,
+    this.genre = '',
+    this.phone = 0,
+    this.email = '',
+    this.code = '',
+    this.availability = '',
+  });
+}
