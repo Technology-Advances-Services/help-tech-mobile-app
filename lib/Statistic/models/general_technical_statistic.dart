ded43910a7125ed556b9c7e59f196daaf08a6930
aaron acuña <aaronacu_85@hotmail.com>
2025-05-07 19:43:38 -0500

    feat(statistic): added general technical statistic model.

---
+++ lib/Statistic/models/general_technical_statistic.dart
@@ -0,0 +1,16 @@
+class GeneralTechnicalStatistic {
+
+  int agendasId;
+  double totalIncome;
+  int totalConsumersServed;
+  int totalWorkTime;
+  int totalPendingsJobs;
+
+  GeneralTechnicalStatistic({
+    this.agendasId = 0,
+    this.totalIncome = 0,
+    this.totalConsumersServed = 0,
+    this.totalWorkTime = 0,
+    this.totalPendingsJobs = 0
+  });
+}
