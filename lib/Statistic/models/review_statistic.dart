6d001415f571e3805f10a9239f5426fdfd5621ed
aaron acuña <aaronacu_85@hotmail.com>
2025-05-08 01:19:12 -0500

    fix(statistic): added review statistic model v2.

---
+++ lib/Statistic/models/review_statistic.dart
@@ -0,0 +1,38 @@
+class ReviewStatistic {
+
+  Map<int, int> scores;
+  int averageScore;
+
+  ReviewStatistic({
+    required this.scores,
+    required this.averageScore
+  });
+
+  factory ReviewStatistic.fromJson(Map<String, dynamic> json) {
+
+    int average = json['AverageScore'];
+
+    Map<int, int> scoreMap = {};
+
+    json.forEach((key, value) {
+      if (key != 'AverageScore') {
+        scoreMap[int.parse(key)] = value;
+      }
+    });
+
+    return ReviewStatistic(scores: scoreMap, averageScore: average);
+  }
+
+  Map<String, dynamic> toJson() {
+
+    final Map<String, dynamic> data = {};
+
+    scores.forEach((key, value) {
+      data[key.toString()] = value;
+    });
+
+    data['AverageScore'] = averageScore;
+
+    return data;
+  }
+}
