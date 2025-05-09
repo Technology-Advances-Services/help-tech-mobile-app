968a85bbb6e77d69ad7b92a57f17ecd94375b9a9
aaron acuña <aaronacu_85@hotmail.com>
2025-05-09 11:59:23 -0500

    feat(attention): added methods for operation.

--- lib/Attention/services/job_service.dart
+++ lib/Attention/services/job_service.dart
@@ -11,6 +11,108 @@ class JobService {
 
   final _storage = const FlutterSecureStorage();
 
+  Future<bool> registerRequestJob(Job job) async {
+
+    var token = await _storage.read(key: 'token');
+
+    token = token?.replaceAll('"', '');
+
+    final response = await http.post(
+      Uri.parse('${_baseUrl}jobs/register-request-job'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({
+        'agendaId': job.agendaId,
+        'consumerId': job.personId,
+        'address': job.address,
+        'description': job.description
+      }),
+    );
+
+    if (response.statusCode >= 200 &&
+        response.statusCode < 300) {
+
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<bool> assignJobDetail(Job job) async {
+
+    var token = await _storage.read(key: 'token');
+
+    token = token?.replaceAll('"', '');
+
+    var response = await http.post(
+      Uri.parse('${_baseUrl}jobs/assign-job-detail'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({
+        'id': job.id,
+        'workDate': job.workDate,
+        'time': job.time,
+        'laborBudget': job.laborBudget,
+        'materialBudget': job.materialBudget
+      }),
+    );
+
+    if (response.statusCode >= 200 &&
+        response.statusCode < 300) {
+
+      response = await http.post(
+        Uri.parse('${_baseUrl}jobs/update-job-state'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+        body: json.encode({
+          'id': job.id,
+          'jobState': job.jobState
+        }),
+      );
+
+      if (response.statusCode >= 200 &&
+          response.statusCode < 300) {
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  Future<bool> completeJob(Job job) async {
+
+    var token = await _storage.read(key: 'token');
+
+    token = token?.replaceAll('"', '');
+
+    final response = await http.post(
+      Uri.parse('${_baseUrl}jobs/update-job-state'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({
+        'id': job.agendaId,
+        'jobState': job.jobState
+      }),
+    );
+
+    if (response.statusCode >= 200 &&
+        response.statusCode < 300) {
+
+      return true;
+    }
+
+    return false;
+  }
+
   Future<List<Job>> jobsByTechnical() async {
 
     var token = await _storage.read(key: 'token');
@@ -61,6 +163,7 @@ class JobService {
 
         final job = Job(
           id: jobJson['id'],
+          agendaId: jobJson['agendasId'],
           registrationDate: DateTime.parse(jobJson['registrationDate']),
           personId: consumerJson['id'],
           firstName: consumerJson['firstname'],
@@ -91,8 +194,7 @@ class JobService {
 
       return jobResults;
     }
-    else {
-      return [];
-    }
+
+    return [];
   }
 }
