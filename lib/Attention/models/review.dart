9141b333511945c8d54469c7f3d14413f12ef8c3
aaron acuña <aaronacu_85@hotmail.com>
2025-05-12 16:48:18 -0500

    feat(attention): added review model.

---
+++ lib/Attention/models/review.dart
@@ -0,0 +1,14 @@
+class Review {
+
+  String technicalId;
+  String consumerId;
+  int score;
+  String opinion;
+
+  Review({
+    this.technicalId = '',
+    this.consumerId = '',
+    this.score = 0,
+    this.opinion = ''
+  });
+}
