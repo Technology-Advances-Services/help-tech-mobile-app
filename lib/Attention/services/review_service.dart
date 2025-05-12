e195605433e7d81ac080d5ba029775834e066ffd
aaron acuña <aaronacu_85@hotmail.com>
2025-05-12 16:48:49 -0500

    feat(attention): added review service.

---
+++ lib/Attention/services/review_service.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/review.dart';
+
+class ReviewService {
+
+  final String _baseUrl = 'http://helptechservice.runasp.net/api/';
+
+  final _storage = const FlutterSecureStorage();
+
+  Future<bool> registerReview(Review review) async {
+
+    var token = await _storage.read(key: 'token');
+
+    token = token?.replaceAll('"', '');
+
+    var username = await _storage.read(key: 'username');
+
+    final response = await http.post(
+      Uri.parse('${_baseUrl}reviews/add-review-to-job'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: json.encode({
+        'technicalId': review.technicalId,
+        'consumerId': username,
+        'score': review.score,
+        'opinion': review.opinion
+      }),
+    );
+
+    if (response.statusCode >= 200 &&
+        response.statusCode < 300) {
+
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<List<Review>> reviewsByTechnical(String technicalId) async {
+
+    var token = await _storage.read(key: 'token');
+
+    token = token?.replaceAll('"', '');
+
+    final response = await http.get(
+      Uri.parse('${_baseUrl}reviews/reviews-by-technical?'
+          'technicalId=$technicalId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      }
+    );
+
+    if (response.statusCode >= 200 &&
+        response.statusCode < 300) {
+
+      List<dynamic> data = json.decode(response.body);
+
+      List<Review> reviews = data.map((parameter) => Review(
+        score: parameter['score'],
+        opinion: parameter['opinion']
+      )).toList();
+
+      return reviews;
+    }
+
+    return [];
+  }
+}
