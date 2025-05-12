4c495dfe599585b19a8e47a5f56663c05271ec9e
aaron acuña <aaronacu_85@hotmail.com>
2025-05-12 16:46:44 -0500

    feat(IAM): added loading view.

--- lib/IAM/views/login.dart
+++ lib/IAM/views/login.dart
@@ -6,7 +6,6 @@ import 'package:helptechmobileapp/Shared/widgets/error_dialog.dart';
 import 'package:helptechmobileapp/Technical/views/interface_technical.dart';
 
 class Login extends StatefulWidget {
-
   const Login({super.key});
 
   @override
@@ -14,19 +13,66 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   final LoginService _loginService = LoginService();
-
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
 
   @override
   void dispose() {
-
     _usernameController.dispose();
     _passwordController.dispose();
-
     super.dispose();
+  }
+
+  Future<void> handleLogin(String role) async {
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username != "" && password != "" && role != "") {
+
+      var result = await _loginService.accessToApp(username, password, role);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (result == true) {
+        Widget nextScreen = role == 'TECNICO'
+            ? const InterfaceTechnical()
+            : const InterfaceConsumer();
+
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => nextScreen),
+              (route) => false,
+        );
+      }
+      else {
+        showDialog(
+          context: context,
+          builder: (context) =>
+          const ErrorDialog(message: 'Credenciales inválidas'),
+        );
+      }
+    }
+    else {
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      showDialog(
+        context: context,
+        builder: (context) =>
+        const ErrorDialog(message: 'Credenciales inválidas'),
+      );
+    }
   }
 
   @override
@@ -53,7 +99,8 @@ class _LoginState extends State<Login> {
                     margin: const EdgeInsets.symmetric(horizontal: 30),
                     color: Colors.white.withOpacity(0.9),
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 30, horizontal: 20),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -74,7 +121,7 @@ class _LoginState extends State<Login> {
                           ),
                           const SizedBox(height: 20),
                           Image.asset(
-                            'assets/IAM/home_logo.PNG',
+                            'assets/IAM/home_logo.png',
                             width: 100,
                             height: 100,
                           ),
@@ -111,110 +158,50 @@ class _LoginState extends State<Login> {
                             ),
                           ),
                           const SizedBox(height: 25),
-                          ElevatedButton(
-                            onPressed: () async {
 
-                              String username = _usernameController.text;
-                              String password = _passwordController.text;
-                              String role = 'TECNICO';
-
-                              if (username != "" &&
-                                  password != "" &&
-                                  role != "") {
-
-                                var result = await _loginService.accessToApp(
-                                    username, password, role);
-
-                                if (result == true) {
-                                  Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(builder: (context) =>
-                                    const InterfaceTechnical()),
-                                        (route) => false,
-                                  );
-                                }
-                                else {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => const ErrorDialog
-                                        (message: 'Credenciales inválidas')
-                                  );
-                                }
-                              }
-                              else {
-                                showDialog(
-                                    context: context,
-                                    builder: (context) => const ErrorDialog
-                                      (message: 'Credenciales inválidas')
-                                );
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.teal,
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
+                          if (_isLoading)
+                            const CircularProgressIndicator()
+                          else ...[
+                            ElevatedButton(
+                              onPressed: () async {
+                                await handleLogin('TECNICO');
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.teal,
+                                foregroundColor: Colors.white,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
                               ),
+                              child: const Text('Técnico'),
                             ),
-                            child: const Text('Técnico'),
-                          ),
-                          const SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: () async {
-
-                              String username = _usernameController.text;
-                              String password = _passwordController.text;
-                              String role = 'CONSUMIDOR';
-
-                              if (username != "" &&
-                                  password != "" &&
-                                  role != "") {
-
-                                var result = await _loginService.accessToApp(
-                                    username, password, role);
-
-                                if (result == true) {
-                                  Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(builder: (context) =>
-                                    const InterfaceConsumer()),
-                                        (route) => false,
-                                  );
-                                }
-                                else {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => const ErrorDialog
-                                        (message: 'Credenciales inválidas')
-                                  );
-                                }
-                              }
-                              else {
-                                showDialog(
-                                    context: context,
-                                    builder: (context) => const ErrorDialog
-                                      (message: 'Credenciales inválidas')
-                                );
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.teal,
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
+                            const SizedBox(height: 10),
+                            ElevatedButton(
+                              onPressed: () async {
+                                await handleLogin('CONSUMIDOR');
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.teal,
+                                foregroundColor: Colors.white,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
                               ),
+                              child: const Text('Consumidor'),
                             ),
-                            child: const Text('Consumidor'),
-                          ),
+                          ],
+
                           const SizedBox(height: 20),
                           GestureDetector(
                             onTap: () {
-                              Navigator.push(
-                                context,
+                              Navigator.push(context,
                                 MaterialPageRoute(builder: (context) =>
-                                const TermsAndConditions()),
+                                    const TermsAndConditions()
+                                ),
                               );
                             },
                             child: const Text(
@@ -225,7 +212,6 @@ class _LoginState extends State<Login> {
                               ),
                             ),
                           ),
-
                         ],
                       ),
                     ),
