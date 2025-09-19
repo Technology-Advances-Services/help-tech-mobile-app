a97c1af951c8d47bb76083f98942437c0b15d0c0
Claudio Sandro Quispesivana <u202215099@upc.edu.pe>
2025-09-19 12:11:10 -0500

    feat(payment): added qr technical model.

---
+++ lib/Payment/models/qr_technical.dart
@@ -0,0 +1,16 @@
+class QrTechnical {
+
+  int id;
+  String? technicalId;
+  DateTime? registrationDate;
+  String qrUrl;
+  String state;
+
+  QrTechnical({
+    this.id = 0,
+    this.technicalId = '',
+    this.registrationDate,
+    this.qrUrl = '',
+    this.state = ''
+  });
+}
